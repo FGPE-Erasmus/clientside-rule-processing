3d09724bb2885326ec1b97db3a73fdb951683079
Wiktor Przetacznik <wiktorprzetacznik@outlook.com>
2025-01-22 20:08:41 +0100

    Let the public API fn be executed as JS Promise

--- browser/src/lib.rs
+++ browser/src/lib.rs
@@ -1,18 +1,26 @@
-use common::event::Event;
 use processor::core::State;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_web::MakeWebConsoleWriter;
+use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 mod net;
 mod parsing;
 
 #[wasm_bindgen]
-pub fn run(event: Event, server_url: &str, player_registration_id: i32) {
+pub async fn run(js_event: JsValue, server_url: &str, player_registration_id: i32) {
+    let event = match serde_wasm_bindgen::from_value(js_event) {
+        Ok(e) => e,
+        Err(err) => {
+            tracing::error!("could not deserialize event, aborting - details {err}");
+            return;
+        }
+    };
+
     init_tracing();
 
-    let potential_game_state = get_game_state(server_url);
+    let potential_game_state = get_game_state(server_url).await;
     if let Err(err) = potential_game_state {
         tracing::error!("could not get game state, aborting - details {err}");
         return;
@@ -21,20 +29,20 @@ pub fn run(event: Event, server_url: &str, player_registration_id: i32) {
     let _ = game_state.update(&event);
     let saving_result = net::upload_game_state(
         game_state.save().as_str(), &game_state.save(), player_registration_id
-    );
+    ).await;
     if let Err(err) = saving_result {
         tracing::error!("could not save game state, aborting - details {err}");
     }
 }
 
-fn get_game_state(server_url: &str) -> anyhow::Result<State> {
-    let state = if let Some(state_str) = net::download_game_state(server_url)? {
+async fn get_game_state(server_url: &str) -> anyhow::Result<State> {
+    let state = if let Some(state_str) = net::download_game_state(server_url).await? {
         State::load(state_str.as_str())
     } else {
         State::new(
-            parsing::parse_simple_rules(net::download_simple_rules(server_url)?.as_str())?,
-            parsing::parse_compound_rules(net::download_compound_rules(server_url)?.as_str())?,
-            parsing::parse_rule_results(net::download_rule_results(server_url)?.as_str())?,
+            parsing::parse_simple_rules(net::download_simple_rules(server_url).await?.as_str())?,
+            parsing::parse_compound_rules(net::download_compound_rules(server_url).await?.as_str())?,
+            parsing::parse_rule_results(net::download_rule_results(server_url).await?.as_str())?,
         )
     };
     Ok(state)
--- browser/src/net.rs
+++ browser/src/net.rs
@@ -6,16 +6,17 @@ use crate::net::model::{ApiResponse, GetCourseDataResponse, LoadGameResponse, Sa
 mod error;
 mod model;
 
-pub(super) fn upload_game_state(server_url: &str, game_state: &str, player_registration_id: i32) -> anyhow::Result<()> {
+pub(super) async fn upload_game_state(server_url: &str, game_state: &str, player_registration_id: i32) -> anyhow::Result<()> {
     let url = format!("{server_url}/save_game");
     let payload = SaveGamePayload {
         player_registration_id,
         game_state: game_state.to_string()
     };
-    let res = reqwest::blocking::Client::new()
+    let res = reqwest::Client::new()
         .post(url)
         .json(&payload)
-        .send()?;
+        .send()
+        .await?;
     if res.status().is_success() {
         Ok(())
     } else {
@@ -23,10 +24,11 @@ pub(super) fn upload_game_state(server_url: &str, game_state: &str, player_regis
     }
 }
 
-pub(super) fn download_game_state(server_url: &str) -> anyhow::Result<Option<String>> {
+pub(super) async fn download_game_state(server_url: &str) -> anyhow::Result<Option<String>> {
     let url = format!("{server_url}/load_game");
-    let response = reqwest::blocking::get(url)?
-        .json::<ApiResponse<LoadGameResponse>>()?;
+    let response = reqwest::get(url).await?
+        .json::<ApiResponse<LoadGameResponse>>()
+        .await?;
     if response.status_code != 200 {
         Err(ResponseError::new(response.status_code))?
     } else {
@@ -41,22 +43,23 @@ pub(super) fn download_game_state(server_url: &str) -> anyhow::Result<Option<Str
     }
 }
 
-pub(super) fn download_simple_rules(server_url: &str) -> anyhow::Result<String> {
-    Ok(download_course_data(server_url)?.course_gamification_rule_conditions)
+pub(super) async fn download_simple_rules(server_url: &str) -> anyhow::Result<String> {
+    Ok(download_course_data(server_url).await?.course_gamification_rule_conditions)
 }
 
-pub(super) fn download_compound_rules(server_url: &str) -> anyhow::Result<String> {
-    Ok(download_course_data(server_url)?.gamification_complex_rules)
+pub(super) async fn download_compound_rules(server_url: &str) -> anyhow::Result<String> {
+    Ok(download_course_data(server_url).await?.gamification_complex_rules)
 }
 
-pub(super) fn download_rule_results(server_url: &str) -> anyhow::Result<String> {
-    Ok(download_course_data(server_url)?.gamification_rule_results)
+pub(super) async fn download_rule_results(server_url: &str) -> anyhow::Result<String> {
+    Ok(download_course_data(server_url).await?.gamification_rule_results)
 }
 
-fn download_course_data(server_url: &str) -> anyhow::Result<GetCourseDataResponse> {
+async fn download_course_data(server_url: &str) -> anyhow::Result<GetCourseDataResponse> {
     let url = format!("{server_url}/get_course_data");
-    let response = reqwest::blocking::get(url)?
-        .json::<ApiResponse<GetCourseDataResponse>>()?;
+    let response = reqwest::get(url).await?
+        .json::<ApiResponse<GetCourseDataResponse>>()
+        .await?;
     if response.status_code != 200 {
         Err(ResponseError::new(response.status_code))?
     } else {
