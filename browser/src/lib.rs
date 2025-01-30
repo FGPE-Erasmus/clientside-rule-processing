ee8a7504fa54d9377c2baee838bea9808abcddeb
Wiktor Przetacznik <wiktorprzetacznik@outlook.com>
2025-01-31 00:40:39 +0100

    Improve error handling in process_event

--- browser/src/lib.rs
+++ browser/src/lib.rs
@@ -22,14 +22,28 @@ pub async fn process_event(js_input: JsValue) -> Option<JsValue> {
     };
 
     let mut state = if input.game_state.is_empty() {
-        State::new(
-            parsing::parse_simple_rules(input.simple_rules_str.as_str())
-                .expect("invalid simple rules string, can't parse"),
-            parsing::parse_compound_rules(input.compound_rules_str.as_str())
-                .expect("invalid compound rules string, can't parse"),
-            parsing::parse_rule_results(input.rule_results_str.as_str())
-                .expect("invalid rule results string, can't parse"),
-        )
+        let sr = match parsing::parse_simple_rules(input.simple_rules_str.as_str()) {
+            Ok(val) => val,
+            Err(err) => {
+                tracing::error!("invalid simple rules string - details {err}");
+                return None;
+            }
+        };
+        let cr = match parsing::parse_compound_rules(input.compound_rules_str.as_str()) {
+            Ok(val) => val,
+            Err(err) => {
+                tracing::error!("invalid compound rule string - details {err}");
+                return None;
+            }
+        };
+        let rr = match parsing::parse_rule_results(input.rule_results_str.as_str()) {
+            Ok(val) => val,
+            Err(err) => {
+                tracing::error!("invalid rule results string - details {err}");
+                return None;
+            }
+        };
+        State::new(sr, cr, rr)
     } else {
         State::load(input.game_state.as_str())
     };
